f4efc8624f90face61ec0c82d5af3b014b7264d5
Kristoffer Dalby <kradalby@kradalby.no>
2015-02-18 16:33:37 +0100

    implements interfjas

--- apollo-zeromq/src/main/java/no/ntnu/apollo/zeromq/ZeroMQProtocolCodecFactory.java
+++ apollo-zeromq/src/main/java/no/ntnu/apollo/zeromq/ZeroMQProtocolCodecFactory.java
@@ -1,5 +1,35 @@
 package no.ntnu.apollo.zeromq;
 
-public class ZeroMQProtocolCodecFactory {
+import org.apache.activemq.apollo.broker.Connector;
+import org.apache.activemq.apollo.broker.protocol.ProtocolCodecFactory;
+import org.fusesource.hawtbuf.Buffer;
+import org.fusesource.hawtdispatch.transport.ProtocolCodec;
+
+public class ZeroMQProtocolCodecFactory implements ProtocolCodecFactory.Provider {
+
+    @Override
+    public String id() {
+        return null;
+    }
+
+    @Override
+    public ProtocolCodec createProtocolCodec(Connector connector) {
+        return null;
+    }
+
+    @Override
+    public boolean isIdentifiable() {
+        return false;
+    }
+
+    @Override
+    public int maxIdentificaionLength() {
+        return 0;
+    }
+
+    @Override
+    public boolean matchesIdentification(Buffer buffer) {
+        return false;
+    }
 
 }
